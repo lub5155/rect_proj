a7d975a04aeb0ee82ea255f91e9ff5cb520341d6
lub5155 <vtarun2004@gmail.com>
2025-12-05 20:44:01 +0530

    .

--- proj/src/components/Ghead.jsx
+++ proj/src/components/Ghead.jsx
@@ -2,7 +2,12 @@ import React from "react";
 function Acord(){
     return <>
         <div id="gameTitle">
-        <h1>yes!</h1>
+        <h1>🎮Memory cards!🃏</h1>
+        <div id="scr">
+            <h4>Score: </h4>
+            <h4>Moves: </h4>
+        </div>
+            <button id="newg">New Game</button>
         </div>
     </>
 }
